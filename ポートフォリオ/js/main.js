78c1bf78aa365c681a4d249e6d122f575578c25d
Tomomitonny <tomtommy23218@gmail.com>
2024-02-06 22:12:55 +0900

    update

--- ポートフォリオ/js/main.js
+++ ポートフォリオ/js/main.js
@@ -1,66 +1,11 @@
+document.getElementById('mobile-menu').addEventListener('click', function() {
+  document.getElementById('navi').classList.toggle('menu-open');
+});
+
+
+
 $(function(){
-  /*=================================================
-  スマホメニュー
-  ===================================================*/
-  // ハンバーガーメニューのクリックイベント
-  // 解説は、「中級編：ストアサイト（インテリア）」参照
-  $('.toggle_btn').on('click', function() {
-    if ($('#header').hasClass('open')) {
-      $('#header').removeClass('open');
-    } else {
-      $('#header').addClass('open');
-    }
-  });
-
-  // #maskのエリアをクリックした時にメニューを閉じる
-  $('#mask').on('click', function() {
-    $('#header').removeClass('open');
-  });
-
-  // リンクをクリックした時にメニューを閉じる
-  $('#navi a').on('click', function() {
-    $('#header').removeClass('open');
-  });
-
-  /*=================================================
-  スムーススクロール
-  ===================================================*/
-  // ページ内リンクのイベント
-  $('a[href^="#"]').click(function(){
-    // リンクを取得
-    let href= $(this).attr("href");
-    // ジャンプ先のid名をセット
-    let target = $(href == "#" || href == "" ? 'html' : href);
-    // トップからジャンプ先の要素までの距離を取得
-    let position = target.offset().top;
-    // animateでスムーススクロールを行う
-    // 600はスクロール速度で単位はミリ秒
-    $("html, body").animate({scrollTop:position}, 600, "swing");
-    return false;
-  });
-
-  /*=================================================
-  PICK UP スライダー
-  ===================================================*/
-  // カルーセル用 jQueryプラグイン「slick」の設定
-  // マニュアル：https://kenwheeler.github.io/slick/
-  $('.slick-area').slick({
-    arrows: false,
-    centerMode: true,
-    centerPadding: '100px',
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          centerPadding: '50px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-
- 
+  
   /*=================================================
   スクロール時の画像フェード表示
   ===================================================*/
